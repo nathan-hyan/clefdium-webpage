c601074d202871882e716a5061723324c83eb04f
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2021-05-26 16:00:36 -0300

    Reverted home changes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
-import WhoAreWe from './components/WhoAreWe';
-// import styles from './styles.module.scss';
+import styles from './styles.module.scss';
 
 function Home() {
-  // const { t } = useTranslation('Home');
+  const { t } = useTranslation('Home');
 
-  return <WhoAreWe />;
+  return <h1 className={styles.foo}>{t('foo')}</h1>;
 }
 export default Home;
