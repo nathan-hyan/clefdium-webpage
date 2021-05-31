1b81757233c968b1b63e751d25d9c9e4b106b68c
JaqKent <jaq.kent@gmail.com>
2021-05-31 20:53:44 -0300

    aboutUs-comments

--- src/screens/Home/components/AboutUs/i18n.ts
+++ src/screens/Home/components/AboutUs/i18n.ts
@@ -3,12 +3,12 @@ import i18next from 'i18next';
 i18next.addResources('es', 'Comments', {
   title: 'QUÉ DICEN NUESTROS SOCIOS',
   one: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, \nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n Ut enim ad minim veniam, quis nostrud exercitation',
-  nameOne: ' John Doe',
+  nameOne: 'John Doe',
   two: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, \nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n Ut enim ad minim veniam, quis nostrud exercitation \nullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-  nameTwo: ' John Doe',
+  nameTwo: 'John Doe',
   three:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, \nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  nameThree: ' John Doe'
+  nameThree: 'John Doe'
 });
 
 i18next.addResources('en', 'Comments', {
