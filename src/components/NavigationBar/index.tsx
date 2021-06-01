e8c15c311a8c857d7df2f8efbfce145d54119c4f
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2021-06-01 02:00:40 -0300

    Added 'selected' prop

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,8 +1,9 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import logo from 'assets/logo.png';
 import Button from 'components/Button';
+import { LangContext } from 'contexts/Language';
 
 import styles from './styles.module.scss';
 import CustomSelect from './components/CustomSelect';
@@ -13,6 +14,7 @@ interface Props {
 }
 
 function NavigationBar({ changeLanguage }: Props) {
+  const { language } = useContext(LangContext);
   const { t } = useTranslation('NavigationBar');
 
   const handleLanguageChange = ({ target: { value } }: ChangeEvent<HTMLSelectElement>) => {
@@ -25,7 +27,12 @@ function NavigationBar({ changeLanguage }: Props) {
         <img src={logo} alt="Clefdium" className={styles.logo} />
       </div>
       <div className={styles.rightSide}>
-        <CustomSelect options={LANGUAGES} translationKey="none" onChange={handleLanguageChange} />
+        <CustomSelect
+          options={LANGUAGES}
+          selected={language}
+          translationKey="none"
+          onChange={handleLanguageChange}
+        />
         <Button label={t('register')} onClick={() => console.log('Thing')} />
         <Button label={t('login')} onClick={() => console.log('Thing')} />
       </div>
