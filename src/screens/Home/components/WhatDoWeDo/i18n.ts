5c840bb1a778c60c646bb391829d0b8b24931805
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2021-06-01 01:53:14 -0300

    Fixed translation and white-space bug

--- src/screens/Home/components/WhatDoWeDo/i18n.ts
+++ src/screens/Home/components/WhatDoWeDo/i18n.ts
@@ -2,7 +2,7 @@ import i18next from 'i18next';
 
 i18next.addResources('es', 'WhatDoWeDo', {
   title: '¿Qué hacemos?',
-  oneTitle: 'Hacemos rendir al máximo tus ahorros ',
+  oneTitle: 'Hacemos rendir al máximo tus ahorros',
   oneText: 'Genera dinero ingresos activos con tus pasivos,\nque crezcas junto a nosotros.',
   twoTitle: 'Gana retornos mensuales de 1,5% desde\nUSD 500 en dólares',
   twoText: 'Esto significa que te ofrecemos un retorno anual\npromedio de un 18% de tu inversión inicial.'
@@ -10,8 +10,8 @@ i18next.addResources('es', 'WhatDoWeDo', {
 
 i18next.addResources('en', 'WhatDoWeDo', {
   title: 'What do we do?',
-  oneTitle: 'text',
-  oneText: 'text',
-  twoTitle: 'text',
-  twoText: 'text'
+  oneTitle: 'We make the most of your savings ',
+  oneText: 'Generate money active income with your liabilities,\nso you grow together with us. ',
+  twoTitle: 'Earn 1.5% monthly returns from USD 500 in dollars',
+  twoText: 'This means that we offer you an average\nannual return of 18% on your initial investment. '
 });
